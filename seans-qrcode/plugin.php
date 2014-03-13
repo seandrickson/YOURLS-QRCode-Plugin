b3f9e614e7498cd3aff85370f0d942da123d33dc
Sean <seandrickson@gmail.com>
2014-03-13 16:54:01 -0700

    Plugin code updated to include all configurable options.
    Also updated some documentation in code to be easier to read.

--- seans-qrcode/plugin.php
+++ seans-qrcode/plugin.php
@@ -3,7 +3,7 @@
 Plugin Name: Sean's QR Code Short URLs
 Plugin URI: https://github.com/seandrickson/YOURLS-QRCode-Plugin
 Description: Allows you to get the QR code by simply clicking on a button in the Admin area (or by adding <tt>.qr</tt> to the end of the keyword.) Works with <a href="https://github.com/seandrickson/YOURLS-Case-Insensitive">Case-Insensitive</a> to create smaller QR codes.
-Version: 1.0
+Version: 1.1
 Author: Sean Hendrickson
 Author URI: https://github.com/seandrickson
 */
@@ -19,23 +19,42 @@ require_once( dirname(__FILE__).'/phpqrcode.php' );
 // Kick in if the loader does not recognize a valid pattern
 yourls_add_action( 'loader_failed', 'sean_yourls_qrcode' );
 function sean_yourls_qrcode( $request ) {
-	$size = '150x150'; // Size of QR code
-	
+	// --- START configurable variables ---
+
+	// output file name, if false outputs to browser with required headers:
+	$outfile = false;
+
+	// error correction level (constants, don't use quotes):
+	$level = QR_ECLEVEL_L; // QR_ECLEVEL_L, QR_ECLEVEL_M, QR_ECLEVEL_Q or QR_ECLEVEL_H
+
+	// pixel size multiplier (3 = 3x3 pixels for QR):
+	$size = 3;
+
+	// outside margin in 'virtual' pixels:
+	$margin = 4;
+
+	// if true code is outputed to browser and saved to file,
+	// otherwise only saved to file.
+	// It is effective only if $outfile is specified.
+	$saveandprint = false;
+
+	// --- END configurable variables ---
+
 	// Get authorized charset in keywords and make a regexp pattern
 	$pattern = yourls_make_regexp_pattern( yourls_get_shorturl_charset() );
-	
-	// Shorturl is like bleh.qr ?
+
+	// if the shorturl is like bleh.qr...
 	if( preg_match( "@^([$pattern]+)\.qr?/?$@", $request[0], $matches ) ) {
 
-		// this shorturl exists ?
+		// if this shorturl exists...
 		$keyword = yourls_sanitize_keyword( $matches[1] );
 		if( yourls_is_shorturl( $keyword ) ) {
 			$url = yourls_link( $keyword );
 			$yourls_url = yourls_site_url( false );
-			
+
 			// If Case-Insensitive plugin is enabled and YOURLS is not a sub-directory install...
-			if( yourls_is_active_plugin( 'case-insensitive/plugin.php' ) 
-				&& ( 'http://'.$_SERVER['HTTP_HOST'] == $yourls_url 
+			if( yourls_is_active_plugin( 'case-insensitive/plugin.php' )
+				&& ( 'http://'.$_SERVER['HTTP_HOST'] == $yourls_url
 				||  'https://'.$_SERVER['HTTP_HOST'] == $yourls_url ) ) {
 
 				// Make the QR smaller
@@ -43,13 +62,13 @@ function sean_yourls_qrcode( $request ) {
 				// http://en.wikipedia.org/wiki/QR_code#Storage
 				$url = strtoupper( $url );
 			}
-			
+
 			// Show the QR code then!
-			QRcode::png( $url );
+			QRcode::png( $url, $outfile, $level, $size, $margin, $saveandprint );
 			exit;
 		}
 	}
-} 
+}
 
 
 // Add our QR Code Button to the Admin interface
@@ -57,11 +76,11 @@ yourls_add_filter( 'action_links', 'sean_add_qrcode_button' );
 function sean_add_qrcode_button( $action_links, $keyword, $url, $ip, $clicks, $timestamp ) {
 	$surl = yourls_link( $keyword );
 	$id = yourls_string2htmlid( $keyword ); // used as HTML #id
-	
+
 	// We're adding .qr to the end of the URL, right?
 	$qr = '.qr';
 	$qrlink = $surl . $qr;
-	
+
 	// Define the QR Code
 	$qrcode = array(
 		'href'    => $qrlink,
@@ -69,7 +88,8 @@ function sean_add_qrcode_button( $action_links, $keyword, $url, $ip, $clicks, $t
 		'title'   => 'QR Code',
 		'anchor'  => 'QR Code'
 	);
-	
+
+	// Add our QR code generator button to the action links list
 	$action_links .= sprintf( '<a href="%s" id="%s" title="%s" class="%s">%s</a>',
 		$qrlink, $qrcode['id'], $qrcode['title'], 'button button_qrcode', $qrcode['anchor']
 	);
@@ -81,8 +101,12 @@ function sean_add_qrcode_button( $action_links, $keyword, $url, $ip, $clicks, $t
 // Add the CSS to <head>
 yourls_add_action( 'html_head', 'sean_add_qrcode_css_head' );
 function sean_add_qrcode_css_head( $context ) {
+
+	// expose what page we are on
 	foreach($context as $k):
-		if( $k == 'index' ): // If we are on the index page, use this css code for the button
+
+		// If we are on the index page, use this css code for the button
+		if( $k == 'index' ):
 ?>
 <style type="text/css">
 	td.actions .button_qrcode {
